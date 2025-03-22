ba2190380e1b023e7a61890bc88a8f57d6e49e67
Chris <yjqql9jj@anonaddy.com>
2025-03-22 12:28:53 -0400

    The most basic of styles ever

--- includes/head.php
+++ includes/head.php
@@ -5,7 +5,12 @@ if (!isset($title)) {
     $title = "Plain Feeder";
 }
 
+# Get the SHA1 hash of the styles file to generate a unique cache busting query string
+$fingerprint = sha1_file("/var/www/plainfeeder.com/public/assets/css/styles.min.css");
+$css_link = "/assets/css/styles.min.css?v=" . $fingerprint;
+
 ?>
 <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width" />
         <title><?php echo $title; ?></title>
+        <link rel="stylesheet" href="<?php echo $css_link; ?>" />
